ef87786b18fef76ce77563b30b20d142450be9c5
marticobos <32466018+marticobos@users.noreply.github.com>
2018-12-18 20:45:38 +0100

    add tolerance to H verification

--- week1/verify_product_H_affine.m
+++ week1/verify_product_H_affine.m
@@ -2,7 +2,5 @@ function areEqual = verify_product_H_affine(H, H_dec)
 % ToDo: verify that the product of the four previous transformations
 % produces the same matrix H as above
 
-areEqual = isequal(H, H_dec);
-% WARNING: maybe they are not exactly equal due to rounding errors. If
-% so(after visual check), compute difference against a small threshold
-% (1e-7 or similar).
+tolerance = 1e-6;
+areEqual = sum(H(:) - H_composed(:) < tolerance)==numel(H);
