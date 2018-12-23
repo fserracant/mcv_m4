7f3fd41b1afa3dee8caa7b0844f02563b588afed
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2018-12-23 12:21:24 +0100

    Added placeholder for function to implemement 'homography2d.m'

---
+++ week2/homography2d.m
@@ -0,0 +1,6 @@
+function H = homography2d(x1, x2)
+% TODO: implement this function.
+% It should compute the homography based on two sets of points x1, x2
+% (correspondences) between two views of the same scene.
+% x1 and x2 are sets of 4 points randomly sampled out of all the
+% correspondences (see 'ransac_homography_adaptive_loop.m').
