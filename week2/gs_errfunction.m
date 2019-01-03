9c5847d200faa3d6930e62f4902c76f8da3caccc
Jonatan <jonatan@adsmurai.com>
2019-01-03 13:20:27 +0100

    style: add a print and remove extra spaces

--- week2/gs_errfunction.m
+++ week2/gs_errfunction.m
@@ -6,14 +6,11 @@ function E = gs_errfunction(P0, Xobs)
     xhat = [reshape(P0(9+1:end),2,[]);ones(1,size(Xobs,2)/2)];
     xhatp = H*xhat;
      
-
     d1 = computeEuclideanDistance (x,xhat);
     d2 = computeEuclideanDistance (xp,xhatp);
     E = [d1, d2];
 end
 
 function dist = computeEuclideanDistance (x1,x2)
-
     dist = (x1(1,:)./x1(3,:)-x2(1,:)./x2(3,:)).^2+(x1(2,:)./x1(3,:)-x2(2,:)./x2(3,:)).^2;
-
 end
