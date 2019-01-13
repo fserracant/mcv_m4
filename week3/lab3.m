d4baed44762f5ee6c5d19462832b01883debeaaf
Jonatan <jonatan@adsmurai.com>
2019-01-13 12:29:27 +0100

    feat: pick randomly some inliers to plot their epipolar lines

--- week3/lab3.m
+++ week3/lab3.m
@@ -91,36 +91,31 @@ vgg_gui_F(im1rgb, im2rgb, F');
 l2 = F * p1; % epipolar lines in image 2 % ToDo
 l1 = F' * p2; % epipolar lines in image 1 % ToDo
 
-% choose three random indices
-m1 = inliers(10);
-m2 = inliers(20);
-m3 = inliers(30);
+% choose some random indices
+n_inliers = 3;
+inlier = randsample(inliers,n_inliers);
 
 % image 1 (plot the three points and their corresponding epipolar lines)
 figure;
 imshow(im1rgb);
+
 hold on;
-plot(p1(1, m1), p1(2, m1), '+g');
-plot_homog_line(l1(:, m1));
-
-plot(p1(1, m2), p1(2, m2), '+g');
-plot_homog_line(l1(:, m2));
-
-plot(p1(1, m3), p1(2, m3), '+g');
-plot_homog_line(l1(:, m3));
+for n = 1:length(inlier)
+  plot(p1(1, inlier(n)), p1(2, inlier(n)), '+g');
+  plot_homog_line(l1(:, inlier(n)));
+end
+hold off;
 
 % image 2 (plot the three points and their corresponding epipolar lines)
 figure;
 imshow(im2rgb);
+
 hold on;
-plot(p2(1, m1), p2(2, m1), '+g');
-plot_homog_line(l2(:, m1));
-
-plot(p2(1, m2), p2(2, m2), '+g');
-plot_homog_line(l2(:, m2));
-
-plot(p2(1, m3), p2(2, m3), '+g');
-plot_homog_line(l2(:, m3));
+for n = 1:length(inlier)
+  plot(p2(1, inlier(n)), p2(2, inlier(n)), '+g');
+  plot_homog_line(l2(:, inlier(n)));
+end
+hold off;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
